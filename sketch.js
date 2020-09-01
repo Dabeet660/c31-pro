80f025a396d13aceced65d4ea67986e84cf43c86
DabeetDas <46596266+DabeetDas@users.noreply.github.com>
2020-09-01 15:23:17 +0530

    second commit

--- sketch.js
+++ sketch.js
@@ -20,27 +20,29 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  ground = new Ground(600,400,1200,30);
+  ground = new Ground(600,400,1200,8);
 
   for(var k=0;k <= width;k=k+80){
     divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight));
   }
-  
+
   for(var j=40; j <=width;j=j+50){
       plinkos.push(new Plinko(j,75,10))
   }
 
-  if(frameCount%60==0){
-    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
-  }
+
 }
 
 function draw() {
-  background(0,0,0);  
+  background(0,0,0);
   Engine.update(engine);
 
   ground.display();
-  
+
+  if(frameCount%60==0){
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
+
   for(var j=0; j < particles.length; j++){
       particles[j].display();
   }
@@ -55,4 +57,3 @@ function draw() {
 
   drawSprites();
 }
-
